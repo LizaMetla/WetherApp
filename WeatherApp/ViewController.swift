164e7ac63df059e1c09fa738fd8fdfb6ce55aa3e
liza.metla <liza.metla@gmail.com>
2020-06-15 19:53:14 +0300

    gradient background + outlets

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundView.layer.addSublayer(gradientLayer)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        SetBlueGradientBackground()
+    }
+    
+    func SetBlueGradientBackground(){
+        let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
     }
 
 
+    func SetGreyGradientBackground(){
+        let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
+    }
 }
 
